523f0647cba947375d1411a6f418285bc280a9e7
Nazmin <Nazminspnn462@gmail.com>
2022-06-07 10:46:27 +0530

    first commit

---
+++ Static keywords/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Absolute value of -12 =  " + Math.abs(-12));
+        System.out.println("Value of PI = " + Math.PI);
+        System.out.println("Value of E = " + Math.E);
+        System.out.println("2^2 = " + Math.pow(2,2));
+
+	}
+
+}
